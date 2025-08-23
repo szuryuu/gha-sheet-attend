66882b9911db85ca9c8de146ac5c16f4bda32b04
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-23 21:42:55 +0700

    fix: broken import package

--- main.go
+++ main.go
@@ -1,7 +1,126 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+const (
+	spreadsheetID  = "1-pqF4HCNXjvON226ZTlkifF0VKiKPvJkOdOxm9N0kl4"
+	sheetName      = "Shafwan Ilham Dzaky"
+	dateTimeFormat = "2/1/2006"
+)
+
+func sheetService(ctx context.Context) (*sheets.Service, error) {
+	creds_base64 := os.Getenv("GCP_SA_KEY")
+	if creds_base64 == "" {
+		return nil, fmt.Errorf("GCP_SA_KEY environment variable not set")
+	}
+
+	creds_json_str, err := base64.StdEncoding.DecodeString(creds_base64)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding data: %v", err)
+	}
+
+	creds_google, err := google.CredentialsFromJSON(ctx, creds_json_str, sheets.SpreadsheetsScope)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating credentials: %v", err)
+	}
+
+	srv, err := sheets.NewService(ctx, option.WithTokenSource(creds_google.TokenSource))
+	if err != nil {
+		return nil, fmt.Errorf("Error creating service: %v", err)
+	}
+
+	return srv, nil
+}
+
+func getNextRowNumber(service *sheets.Service) (int, error) {
+	readRange := fmt.Sprintf("%s!A1:A", sheetName)
+	resp, err := service.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+	if err != nil {
+		return 0, err
+	}
+
+	if len(resp.Values) <= 4 {
+		return 1, nil
+	}
+
+	lastIndex := -1
+	for i := len(resp.Values) - 1; i >= 0; i-- {
+		if len(resp.Values[i]) > 0 {
+			lastIndex = i
+			break
+		}
+	}
+
+	if lastIndex == -1 {
+		return 1, nil
+	}
+
+	return lastIndex + 2, nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	ctx := context.Background()
+	sheetService, err := sheetService(ctx)
+	if err != nil {
+		fmt.Println("Error creating service:", err)
+		return
+	}
+
+	nextNumberRow, err := getNextRowNumber(sheetService)
+	if err != nil {
+		fmt.Println("Error getting next row number:", err)
+		return
+	}
+	log.Printf("Next number row: %d", nextNumberRow)
+
+	attendRecord := os.Getenv("INPUT_ATTEND_RECORD")
+	startTime := os.Getenv("INPUT_START_TIME")
+	endTime := os.Getenv("INPUT_END_TIME")
+	additionalInfo := os.Getenv("INPUT_ADDITIONAL_INFO")
+
+	if attendRecord == "Libur" {
+		startTime = ""
+		endTime = ""
+	}
+
+	loc, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return
+	}
+
+	todayDate := time.Now().In(loc).Format(dateTimeFormat)
+	newRow := sheets.ValueRange{
+		Values: [][]any{
+			{
+				nextNumberRow,
+				todayDate,
+				attendRecord,
+				startTime,
+				endTime,
+				additionalInfo,
+			},
+		},
+	}
+
+	appendCall := sheetService.Spreadsheets.Values.Append(spreadsheetID, sheetName, &newRow).
+		ValueInputOption("USER_ENTERED")
+	_, err = appendCall.Do()
+	if err != nil {
+		fmt.Println("Error appending row:", err)
+		return
+	}
+
+	log.Printf("Row appended successfully")
 }
